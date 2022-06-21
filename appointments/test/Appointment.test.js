174b4d6615314642ad47340ff62cb8851ebe96cc
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-21 17:27:31 +0100

    test red, appointment renders antoher customer

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import { Appointment } from '../src/Appointment';
 import { render, screen } from '@testing-library/react';
+import {createRoot} from 'react-dom/client';
 
 describe("Appointment", () => {
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
+    const component = <Appointment customer={customer} />;
+    const rootContainer = document.createElement("div");
 
-    render(<Appointment customer={customer} />);
-    expect(document.body.textContent).toMatch("Ashley");
+    const root = createRoot(rootContainer);
+
+    act(() => {
+      root.render(component);
+    })
+
+    expect(rootContainer.textContent).toMatch("Ashley");
+  });
+  it('renders another customer first name', () => {
+    const customer = { firstName: "Jordan" };
+    const component = <Appointment customer={customer} />;
+    const rootContainer = document.createElement("div");
+
+    const root = createRoot(rootContainer);
+    
+    act(() => {
+      root.render(component);
+    })
+    
+    expect(rootContainer.textContent).toMatch("Jordan");
   });
 });
