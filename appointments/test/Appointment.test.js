56ca8154551adb8af1fbe453faedacdb2327023e
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-21 16:40:34 +0100

    test pass, configuration of jest-env..js-dom

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,5 +1,13 @@
-describe('Appointment', () => { 
-    it('renders the customer first name', () => {
-        expect(document.body.textContent).toMatch('Ashley');
-    });
- })
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+import { render, screen } from '@testing-library/react';
+
+describe("Appointment", () => {
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Ashley" };
+
+    render(<Appointment customer={customer} />);
+    expect(document.body.textContent).toMatch("Ashley");
+  });
+});
