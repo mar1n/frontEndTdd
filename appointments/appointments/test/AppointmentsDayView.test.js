480d60380587e93dfb9f6757f8aefe0d37f86b0c
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-25 14:46:34 +0100

    exerice from chapter 1

--- appointments/appointments/test/AppointmentsDayView.test.js
+++ appointments/appointments/test/AppointmentsDayView.test.js
@@ -4,7 +4,7 @@ import ReactTestUtils from 'react-dom/test-utils';
 import {
   Appointment,
   AppointmentsDayView
-} from '../src/Appointment';
+} from '../src/AppointmentsDayView';
 
 describe('Appointment', () => {
   let container;
@@ -17,17 +17,54 @@ describe('Appointment', () => {
   const render = component =>
     ReactDOM.render(component, container);
 
+  const appointmentTable = () => container.querySelector('#appointmentView > table');
+
+  it('renders a table', () => {
+    customer = { firstName: 'Ashley' };
+    render(<Appointment customer={customer} />);
+    expect(appointmentTable().textContent).not.toBeNull();
+  });
+
   it('renders the customer first name', () => {
     customer = { firstName: 'Ashley' };
     render(<Appointment customer={customer} />);
-    expect(container.textContent).toMatch('Ashley');
+    expect(appointmentTable().textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
     customer = { firstName: 'Jordan' };
     render(<Appointment customer={customer} />);
-    expect(container.textContent).toMatch('Jordan');
+    expect(appointmentTable().textContent).toMatch('Jordan');
   });
+  it('renders the customer last name', () => {
+    customer = { lastName: 'Novak'};
+    render(<Appointment customer={customer} />);
+    expect(appointmentTable().textContent).toMatch('Novak');
+  });
+  it('renders the customer phone number', () => {
+    customer = { phoneNumber: '7784701540'};
+    render(<Appointment customer={customer} />);
+    expect(appointmentTable().textContent).toMatch('7784701540');
+  });
+  it('renders stylist name', () => {
+    render(<Appointment customer={customer} stylist="Rocky" />);
+    expect(appointmentTable().textContent).toMatch("Rocky")
+  });
+  it('renders service name', () => {
+    render(<Appointment customer={customer} service="Cut" />);
+    expect(appointmentTable().textContent).toMatch("Cut");
+  });
+  it('renders note description', () => {
+    render(<Appointment customer={customer} notes="abc" />);
+    expect(appointmentTable().textContent).toMatch("abc");
+  });
+  it('renders a heading with the time', () => {
+    const today = new Date();
+    const timestamp = today.setHours(9, 0, 0);
+    render(<Appointment customer={customer} startsAt={timestamp} />);
+    expect(container.querySelector('h3')).not.toBeNull();
+    expect(container.querySelector('h3').textContent).toEqual("Today’s appointment at 09:00")
+  })
 });
 
 describe('AppointmentsDayView', () => {
@@ -102,5 +139,17 @@ describe('AppointmentsDayView', () => {
     const button = container.querySelectorAll('button')[1];
     ReactTestUtils.Simulate.click(button);
     expect(container.textContent).toMatch('Jordan');
+  });
+
+  it('does not show toogle class when button is not cliced', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    const button = container.querySelectorAll('button')[1];
+    expect(button.className).not.toMatch('toggle');
+  });
+  it('button has toogle class after click', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    const button = container.querySelectorAll('button')[1];
+    ReactTestUtils.Simulate.click(button);
+    expect(button.className).not.toMatch('toggled');
   });
 });
