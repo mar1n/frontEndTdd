2f617e5e1c90b88c3333a2195fe882e76f8299bc
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-27 16:44:17 +0100

    spy, stub, forms fn, promises

--- appointments/appointments/src/CustomerForm.js
+++ appointments/appointments/src/CustomerForm.js
@@ -6,20 +6,30 @@ export const CustomerForm = ({
   phoneNumber,
   onSubmit,
   fetch,
+  onSave,
 }) => {
   const [customer, setCustomer] = useState({
     firstName,
     lastName,
     phoneNumber,
   });
+  const [error, setError] = useState(false);
 
-  const handleSubmit = () => {
-    window.fetch('/customers', {
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const result = await window.fetch('/customers', {
       method: 'POST',
       credentials: 'same-origin',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(customer)
+      body: JSON.stringify(customer),
     });
+    if(result.ok) {
+      const customerWithId = await result.json();
+      onSave(customerWithId);
+    }else {
+      setError(true)
+    }
   };
   const handleChange = ({ target }) =>
     setCustomer((customer) => ({
@@ -29,7 +39,9 @@ export const CustomerForm = ({
   return (
     <form
       onChange={handleChange}
-      id="customer" onSubmit={handleSubmit}>
+      id="customer"
+      onSubmit={handleSubmit}>
+        {error ? <Error /> : null}
       <label htmlFor="firstName">First name</label>
       <input
         readOnly
@@ -57,4 +69,9 @@ export const CustomerForm = ({
       <input type="submit" value="Add" />
     </form>
   );
+};
+const Error = () => (<div className="error">An error occurred during save.</div>)
+
+CustomerForm.defaultProps = {
+  onSave: () => {},
 };
