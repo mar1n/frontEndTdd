878c1e972594d81fb5eb4acbe18702f40f173e4b
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-30 00:04:07 +0100

    shallowHelpers done

--- appointments/appointments/test/shallowHelpers.test.js
+++ appointments/appointments/test/shallowHelpers.test.js
@@ -1,6 +1,6 @@
 import { TestEnvironment } from 'jest-environment-jsdom';
 import React from 'react';
-import { childrenOf, createShallowRenderer } from './shallowHelpers';
+import { childrenOf, createShallowRenderer, type } from './shallowHelpers';
 
 describe('childrenOf', () => { 
     it('returns no children', () => {
@@ -43,3 +43,52 @@ describe('childrenOf', () => {
         expect(child(1)).toEqual(<p>B</p>);
     });
  });
+
+describe('elementsMatching', () => {
+    let render, elementsMatching;
+
+    beforeEach(() => {
+        ({ render, elementsMatching} = createShallowRenderer());
+    });
+
+    it('finds multiple direct children', () => {
+        render(
+            <TestComponent>
+                <p>A</p>
+                <p>B</p>
+            </TestComponent>
+        );
+        expect(elementsMatching(type('p'))).toEqual([
+            <p>A</p>,
+            <p>B</p>
+        ])
+    });
+    it('finds indirect children', () => {
+        render(
+            <TestComponent>
+                <div>
+                    <p>A</p>
+                </div>
+            </TestComponent>
+        )
+        expect(elementsMatching(type('p'))).toEqual([<p>A</p>]);
+    });
+});
+
+describe('elementMatching', () => {
+    let render, elementMatching;
+
+    beforeEach(() => {
+        ({ render, elementMatching } = createShallowRenderer());
+    });
+
+    it('finds first direct child', () => {
+        render(
+            <TestComponent>
+                <p>A</p>
+                <p>B</p>
+            </TestComponent>
+        )
+        expect(elementMatching(type('p'))).toEqual(<p>A</p>);
+    });
+});
