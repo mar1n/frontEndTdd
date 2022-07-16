5b2335ed93ff0ce5da905d99e68a9d5da6d750ed
mar1n <30738216+mar1n@users.noreply.github.com>
2022-07-16 21:58:09 +0100

    displaying data fetched from an endpoint

---
+++ appointments/appointments/src/CustomerSearch.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+const CustomerRow = ({ customer }) => (
+    <tr>
+      <td>{customer.firstName}</td>
+      <td>{customer.lastName}</td>
+      <td>{customer.phoneNumber}</td>
+      <td />
+    </tr>
+  );
+
+export const CustomerSearch = () => {
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    const featchData = async () => {
+      const result = await window.fetch('/customers', {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      setCustomers(await result.json());
+    };
+    featchData();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Phone number</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {customers.map(customer => (
+          <CustomerRow customer={customer} key={customer.id} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
