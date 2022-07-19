283f61483a72e1b5a9afc27e33c31d3b57dc5528
mar1n <30738216+mar1n@users.noreply.github.com>
2022-07-19 18:59:38 +0100

    render a router link. router button

--- appointments/appointments/src/customerSearch/RouterButton.js
+++ appointments/appointments/src/customerSearch/RouterButton.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { objectToQueryString } from '../objectToQueryString';
 
-export const RouterButton = ({ pathname, queryParams, disabled }) => {
+export const RouterButton = ({ pathname, queryParams, disabled, children }) => {
     let className = 'button';
     if(disabled) {
         className += ' disabled'
     }
     return(
         <Link
-            className='button'
+            className={className}
             to={{
                 pathname: pathname,
                 search: objectToQueryString(queryParams)
             }}
         >
+            {children}
         </Link>
     )
 };
--- appointments/appointments/test/customerSearch/RouterButton.test.js
+++ appointments/appointments/test/customerSearch/RouterButton.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { createShallowRenderer } from '../shallowHelpers';
 import { RouterButton } from '../../src/CustomerSearch/RouterButton';
 
@@ -20,10 +20,24 @@ describe('RouterButton', () => {
             />
         )
         expect(root().type).toEqual(Link);
-        expect(root().props.className).toEqual('button');
+        expect(root().props.className).toContain('button');
         expect(root().props.to).toEqual({
             pathname: '/path',
             search: '?a=123&b=234'
         })
+    });
+    it('renders children', () => {
+        render(
+            <RouterButton queryParams={queryParams}>
+                child text
+            </RouterButton>
+        );
+        expect(root().props.children).toEqual('child text')
+    });
+    it('adds disabled class if disabled prop is true', () => {
+        render(
+            <RouterButton disabled={true} queryParams={queryParams} />
+        )
+        expect(root().props.className).toContain('disabled')
     });
 });
